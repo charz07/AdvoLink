cee355d0c3be8cbeb5317660824ecc9a9e1fbde4
Charlotte Zhou <charlottezhou6@gmail.com>
2023-10-18 20:13:34 -0700

    Initial Commit

---
+++ testApp/testAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  testAppApp.swift
+//  testApp
+//
+//  Created by Charlotte Zhou on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct testAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
